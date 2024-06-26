88d63145f1199daba2a842005b54f0c2b26c2588
Jackline Amwenya Nehondo <jnehondo@gmail.com>
2024-06-26 08:33:06 +0300

    working with arithmetics in js

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+// equation that will multiply the variables num1 and num2
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+
+// Generate a random integer greater than 0
+const random = Math.floor(Math.random() * 5) + 1;
+
+
+// equation that will calculate the remainder of dividing variable num3 by num4
+const num3 = 9;
+const num4 = 5;
+const mod = num3 % num4;
+
+
+// finds the highest number in a set; the value returned should be 20
+const max = Math.max(3, 4, 8, 15, 20);
+
+console.log(max);
